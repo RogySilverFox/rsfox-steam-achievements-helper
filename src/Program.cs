693413c2cd2effc96547b5edc7a0b78298bebfe5
RSFox <RogySilverFox@gmail.com>
2021-08-03 01:03:19 +0700

    Initial template project plus old template

---
+++ src/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace rsfox_steam_achievements_helper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
